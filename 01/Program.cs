8ed0dec16cc924dd26638981281b78c65d83359b
Franz Hemmer <fhemmer@relias.com>
2025-12-01 11:44:55 -0500

    Day 1 - WIP

---
+++ 01/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+var (pos, count) = (50, 0);
+foreach (var m in Regex.Matches(File.ReadAllText("input.txt"), @"([LR])(\d+)").Cast<Match>())
+{
+    pos = ((pos + (m.Groups[1].Value == "R" ? 1 : -1) * int.Parse(m.Groups[2].Value)) % 100 + 100) % 100;
+    if (pos == 0) count++;
+}
+Console.WriteLine($"Day 01 Answer: {count}");
+
