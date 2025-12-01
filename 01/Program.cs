7026fc35addcbce57902a785ebbbcf2c9b929f41
Franz Hemmer <fhemmer@relias.com>
2025-12-01 11:55:24 -0500

    Day 01 - WIP

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System.Text.RegularExpressions;
 
-var (pos, count) = (50, 0);
-foreach (var m in Regex.Matches(File.ReadAllText("input.txt"), @"([LR])(\d+)").Cast<Match>())
-{
-    pos = ((pos + (m.Groups[1].Value == "R" ? 1 : -1) * int.Parse(m.Groups[2].Value)) % 100 + 100) % 100;
-    if (pos == 0) count++;
-}
+var input = await File.ReadAllTextAsync("input.txt").ConfigureAwait(false);
+var count = RotationRegex().Matches(input)
+    .Select(m => (Dir: m.Groups[1].Value, Dist: int.Parse(m.Groups[2].Value, System.Globalization.CultureInfo.InvariantCulture)))
+    .Aggregate((Pos: 50, Count: 0), (state, r) =>
+    {
+        var newPos = ((state.Pos + (r.Dir == "R" ? r.Dist : -r.Dist)) % 100 + 100) % 100;
+        return (newPos, state.Count + (newPos == 0 ? 1 : 0));
+    }).Count;
+
 Console.WriteLine($"Day 01 Answer: {count}");
+
+static partial class Program
+{
+    [GeneratedRegex(@"([LR])(\d+)")]
+    private static partial Regex RotationRegex();
+}
 
